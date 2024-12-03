70011dc29c85098823c682e5d84b65ce7241c5a5
Abraham Samuel <ebubesammy567@gmail.com>
2024-12-03 13:39:33 +0100

    commit

--- script.js
+++ script.js
@@ -4,20 +4,19 @@ let mainContainer = mk("div", {
   id: "mainContainer",
   style: {
     backgroundColor: "white",
-      padding: "0 10px 0 0",
+      padding: "0 30px 0 0",
       height: "fit-content",
-    borderTop: "1px solid green",
-    borderBottom: "1px solid green",
-      borderRight: "1px solid green",
-    borderRadius: "5px",
+    borderRadius: "10px",
   },
 });
 
 let container = mk("div", {
   id: "container",
   style: {
-      padding: "10px 10px 40px 50px",
-      borderLeft: "5px solid lightblue",
+    padding: "15px 10px 40px 50px",
+    borderLeft: "5px solid blue",
+    margin: "0 auto 0 0",
+    borderTopRightRadius: '10px,'
   },
 });
 
@@ -26,16 +25,19 @@ let title = mk("h1", {
     "See you on the other side, where we will discuss the Events in JavaScript, May God be with you",
   style: {
     color: "black",
-    fontSize: "28px",
+    fontSize: "25px",
       textAlign: "left",
-    padding: "0 0 0 0",
+    padding: "0",
   },
 });
+
 let quoteLeft = mk("svg", { innerHTML: `<i class='bx bxs-quote-alt-left'></i>` });
+
+
 let quoteRight = mk("svg", {
     innerHTML: `<i class='bx bxs-quote-alt-right'></i>`,
     style: {
-        padding: "0 0 0 30px",
+      padding: "20px 0 0 30px",
     }
 });
 
